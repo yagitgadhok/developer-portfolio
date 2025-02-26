e94d474eb00e8043df95ec59ff8a800e2f2c0825
yagitgadhok <gadhokyagit@gmail.com>
2025-02-26 12:57:13 +0530

    new changes done

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,29 +1,24 @@
 export const skillsData = [
-  'HTML',
-  'CSS',
-  'Javascript',
-  'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
-  'Git',
-  'AWS',
-  'Bootstrap',
-  'Docker',
-  'Go',
-  'Figma',
-  'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
-]
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Typescript",
+  "React",
+  "Java",
+  "Next JS",
+  "Node JS",
+  "Angular",
+  "Tailwind",
+  "MongoDB",
+  "Git",
+  "Bootstrap",
+  "Figma",
+  "Firebase",
+  "MaterialUI",
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
